1f08b47157a406fca7fea2ba959007badf44a65a
RPiList <56664851+RPiList@users.noreply.github.com>
2023-09-14 04:01:43 +0200

    Create 005-for-array.py

---
+++ 005-for-array.py
@@ -0,0 +1,13 @@
+# Die FOR-Schleife und Arrays
+
+# Die Variable "einkaufen" ist ein Array.
+# Stell Dir das wie eine Kiste vor, in die Du 
+# mehrere Sachen packen kannst.
+
+einkaufen = ["Joghurt", "Nutella", "Bananen", "Pommes"]
+
+# um die Punkte in dem Array "einkaufen" 
+# alle durchzulaufen, brauchen wir eine FOR-Schleife
+
+for produkt in einkaufen:
+    print("Wir brauchen " + produkt)
