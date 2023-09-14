b04c92fa14092c09bfb875cb11a20e4effd412f5
RPiList <56664851+RPiList@users.noreply.github.com>
2023-09-14 03:36:50 +0200

    Create if-abfrage.py

---
+++ if-abfrage.py
@@ -0,0 +1,16 @@
+# Die IF oder WENN Abfrage
+# Die IF Abfrage wird benutzt, wenn man 
+# einen Befehl nur unter bestimmten Umständen
+# ausführen möchte
+
+a = 6
+if a == 5:
+    print("a ist kleiner als 5")
+else:
+    print("a ist größer als 5")
+
+# Der Befehl print() wird nur ausgefügt,
+# wenn die Bedingung "a kleiner 5" wahr ist.
+
+# Hier gibt es keine Aufgabe. Weiter mit der
+# nächsten Aufgabe
