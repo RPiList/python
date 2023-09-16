010c66a923e2366dd767371b86a89ba210772b0b
RPiList <56664851+RPiList@users.noreply.github.com>
2023-09-16 04:05:02 +0200

    Create 009-string-mani-03.py

---
+++ 009-string-mani-03.py
@@ -0,0 +1,31 @@
+# Nachfolgend wird eine Datei geöffnet und dann
+# Zeilenweise gelesen. Das ist hier NICHT wichtig.
+# Uns geht es nur um die For-Schleife am Ende.
+
+dateiname = 'malware.txt'
+
+counter = 0
+MySet = set()
+MyListe = []
+
+with open(dateiname, "r", encoding='UTF-8') as datei: # "r" steht für Read-Only Zugriff
+    # Datei wird zeilenweise eingelesen
+    for zeile in datei:
+        counter = counter + 1
+        MyListe.append(zeile.strip())
+
+for eintrag in MyListe:
+    tlddot = eintrag.rfind('.')
+    MySet.add(eintrag[tlddot:])
+
+counter = 0
+for seteintrag in MySet:
+    for listeneintrag in MyListe:
+        if str(listeneintrag).endswith(seteintrag):
+            counter = counter + 1
+    print(str(seteintrag) + ": " + str(counter))
+    counter = 0
+    
+print("TLDs insg.: " + str(len(MySet)))
+print("Domains insg.: " + str(len(MyListe)))
+
